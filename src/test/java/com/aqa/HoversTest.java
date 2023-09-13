4bb120a4116a19a535db8afdc906ca9ad2aea50d
aqaacc <mralextb@gmail.com>
2023-09-13 13:15:51 +0200

    added final fix to HoversTest

--- src/test/java/com/aqa/HoversTest.java
+++ src/test/java/com/aqa/HoversTest.java
@@ -3,6 +3,7 @@ package com.aqa;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -15,8 +16,9 @@ public class HoversTest extends BaseTest{
         List<WebElement> avatars = driver.findElements(By.cssSelector("img[alt=\"User Avatar\"]"));
         Actions actions = new Actions(driver);
         actions.moveToElement(avatars.get(0)).build().perform();
+        String elementH5 = driver.findElement(By.xpath(".//h5")).getText();
+        Assert.assertEquals(elementH5, "name: user1", "Assert message");
         driver.findElement(By.linkText("View profile")).click();
         driver.navigate().back();
-
     }
 }
